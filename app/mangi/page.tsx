a9f3066a1f84dda4fce910f6be757edd4e92d4f8
Kusko321 <kusko321@gmail.com>
2025-01-02 08:59:03 +0100

    pierwsze proby

---
+++ app/mangi/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+export default function Page() {
+    return (
+    <div
+        className="grid justify-items-center"
+    >
+        <div
+            className="flex flex-row items-center gap-8"
+        >
+            <Image
+                src="/mangowy-lisek-logo.png"
+                alt="Mangowy Lisek logo"
+                width={80}
+                height={80}
+                priority
+            />
+            <input
+                className="focus\:border-0:focus placeholder-neutral-600 text-gray-50 bg-neutral-800 p-1.5"
+                type="text"
+                placeholder="Podaj nazwe mangi"
+            />
+
+        </div>
+    </div>
+)
+}
