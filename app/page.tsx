f4f191784186560ac79dd8b7ec1ee77575f31507
Kusko321 <kusko321@gmail.com>
2025-01-10 22:02:02 +0100

    mangi

--- app/page.tsx
+++ app/page.tsx
@@ -93,7 +93,7 @@ export default function Page() {
                                         <Image
                                             src={item.img}
                                             alt={item.name}
-                                            width={120} // Szerokość obrazka
+                                            width={120} //  obrazka
                                             height={160} // Wysokość obrazka
                                             className="rounded-lg object-cover"
                                         />
