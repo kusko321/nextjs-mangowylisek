dc5e679fa0794bb7a224ddfd4e40d9c16db23b0c
Kusko321 <kusko321@gmail.com>
2025-05-03 21:49:36 +0200

    mangi

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,6 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://zbsggxpkgwhaoneuxams.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpic2dneHBrZ3doYW9uZXV4YW1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwNTA3NjIsImV4cCI6MjA2MTYyNjc2Mn0.O3NCcw-Z2gQ_fq-FN07Ne9gBCKcTQaG7JJys6TDRpLE';
-const supabase = createClient(supabaseUrl, supabaseKey);
-
 
 type Manga = {
     id: number;
