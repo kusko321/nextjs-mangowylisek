c91183eb11d92ebcf410e70525cbe54200c39ee0
Kusko321 <kusko321@gmail.com>
2025-05-06 22:55:46 +0200

    mangi

--- app/vinted/[id]/page.tsx
+++ app/vinted/[id]/page.tsx
@@ -1,15 +1,11 @@
 
 
-type Props = {
-    params: {
-        id: string;
-    };
-};
+export default function Page({ params }: { params: { id: string } }) {
+    const id = params.id;
 
-export default function ProduktPage({ params }: Props) {
     return (
-        <div className="text-white">
-            <h1>Produkt ID: {params.id}</h1>
+        <div>
+            ID z URL: {id}
         </div>
     );
 }
