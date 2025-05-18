59c6e15fb1ba0149640b8e44a49e663b70a3ef7c
Kusko321 <kusko321@gmail.com>
2025-05-18 21:48:41 +0200

    mangi

--- app/api/supabase.ts
+++ app/api/supabase.ts
@@ -2,4 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+    throw new Error('Brakuje zmiennych środowiskowych SUPABASE!');
+}
 export const supabase = createClient(supabaseUrl, supabaseKey);
