2322cc83d89e3f7a5061a97acf3a39ec4e9f8efb
Kusko321 <kusko321@gmail.com>
2025-05-18 21:45:01 +0200

    mangi

--- app/api/supabase.ts
+++ app/api/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zbsggxpkgwhaoneuxams.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpic2dneHBrZ3doYW9uZXV4YW1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwNTA3NjIsImV4cCI6MjA2MTYyNjc2Mn0.O3NCcw-Z2gQ_fq-FN07Ne9gBCKcTQaG7JJys6TDRpLE';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
--- app/wiktoria/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function PrivatePage() {
-    return <h1>To jest strona prywatna!</h1>
-}
