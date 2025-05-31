b7b79a143911a939394fd45c4c9f130eb7809f45
Kusko321 <kusko321@gmail.com>
2025-05-31 21:26:16 +0200

    mangi

--- app/rocznica/page.tsx
+++ app/rocznica/page.tsx
@@ -1,61 +1,62 @@
 'use client';
 import * as React from "react";
-import {Plane} from 'lucide-react';
+import {Plane, TrainFront } from 'lucide-react';
 export default function Home() {
     return (
         <div className="grid justify-items-center font-sans ">
-            <span className="text-2xl m-5 font-black">Plan podróży z okazji naszej rocznicy ^^</span>
+            <span className="text-2xl m-5 font-black">Plan podróży z okazji naszej rocznicy</span>
+            <div className="m-5 text-lg">
+                Piątek 20 Czerwca
+            </div>
 
-
-            Torun > Krakow
-            <div className="flex flex-row">
-                <div className="m-2 ">
-                    <div className="rounded-t-xl p-1 flex flex-row  items-center justify-center bg-[url(/zadar.jpg)] bg-cover bg-center w-[300px] h-[120px] text-3xl font-black text-center border-1 rounded-4">
-                        <span className="p-3">RZE</span> <Plane strokeWidth={3} />  <span className="p-3">ZAD</span>
+            <div className="m-2">
+                <div className="rounded-t-xl p-1 flex flex-row  items-center justify-center bg-[url(/krakow.jpg)] bg-cover bg-center w-[300px] h-[120px] text-3xl font-black text-center border-1 rounded-4">
+                    <span className="p-3">TRG</span> <TrainFront  strokeWidth={3} />  <span className="p-3">KRK</span>
+                </div>
+                <div className="bg-neutral-950 rounded-b-xl p-1 break-words w-[300px] p-3 border-t border-dashed border-neutral-50">
+                    <div className="pl-3 flex flex-col mt-3 mt-5 tracking-tight">
+                        <span className="text-xl font-medium">Wiktoria Gruszczyńska</span>
+                        <span className="text-xl font-medium">Adam Malinowski</span>
+                        <span className="text-sm">Pasażerowie</span>
                     </div>
-                    <div className="bg-teal-50 text-neutral-950 rounded-b-xl p-1 break-words w-[300px] p-3 border-t border-dashed border-neutral-950">
-                        <div className="pl-3 flex flex-col mt-1">
-                            <span className="text-lg font-medium">Wiktoria Gruszczyńska</span>
-                            <span className="text-sm">Pasażer</span>
-                        </div>
-                        <div className="pl-3 flex flex-col mt-1">
-                            <span className="text-lg font-medium">FR9399</span>
-                            <span className="text-sm">Lot</span>
-                        </div>
-                        <div className="pl-3 flex flex-col mt-1">
-                            <span className="text-lg font-medium">16A</span>
-                            <span className="text-sm">Miejsce</span>
-                        </div>
-                        <div className="pl-3 flex flex-col mt-1">
-                            <span className="text-lg font-medium">21/06/2025  21:50</span>
-                            <span className="text-sm">Data</span>
-                        </div>
+                    <div className="pl-3 flex flex-col mt-3 mt-5 mb-5 tracking-tight">
+                        <span className="text-xl font-medium">20/06/2025</span>
+                        <span className="text-sm">Data</span>
                     </div>
                 </div>
+            </div>
+            <div className="m-5 text-lg">
+                Sobota 21 Czerwca
+            </div>
                 <div className="m-2">
                     <div className="rounded-t-xl p-1 flex flex-row  items-center justify-center bg-[url(/zadar.jpg)] bg-cover bg-center w-[300px] h-[120px] text-3xl font-black text-center border-1 rounded-4">
                         <span className="p-3">RZE</span> <Plane strokeWidth={3} />  <span className="p-3">ZAD</span>
                     </div>
                     <div className="bg-neutral-950 rounded-b-xl p-1 break-words w-[300px] p-3 border-t border-dashed border-neutral-50">
-                        <div className="pl-3 flex flex-col mt-1">
-                            <span className="text-lg font-medium">Adam Malinowski</span>
-                            <span className="text-sm">Pasażer</span>
+                        <div className="pl-3 flex flex-col mt-3 mt-5 tracking-tight">
+                            <span className="text-xl font-medium">Wiktoria Gruszczyńska</span>
+                            <span className="text-xl font-medium">Adam Malinowski</span>
+                            <span className="text-sm">Pasażerowie</span>
                         </div>
-                        <div className="pl-3 flex flex-col mt-1">
-                            <span className="text-lg font-medium">FR9399</span>
+                        <div className="pl-3 flex flex-col mt-3 mt-5 tracking-tight">
+                            <span className="text-xl font-medium">FR9399</span>
                             <span className="text-sm">Lot</span>
                         </div>
-                        <div className="pl-3 flex flex-col mt-1">
-                            <span className="text-lg font-medium">16B</span>
+                        <div className="pl-3 flex flex-col mt-3 mt-5 tracking-tight">
+                            <span className="text-xl spaci font-medium">16A</span>
+                            <span className="text-xl font-medium">16B</span>
                             <span className="text-sm">Miejsce</span>
                         </div>
-                        <div className="pl-3 flex flex-col mt-1">
-                            <span className="text-lg font-medium">21/06/2025  21:50</span>
-                            <span className="text-sm">Data</span>
+                        <div className="pl-3 flex flex-col mt-3 mt-5 mb-5 tracking-tight">
+                            <span className="text-xl font-medium">21/06/2025 21:50</span>
+                            <span className="text-sm">Czas wylotu</span>
+                        </div>
+                        <div className="pl-3 flex flex-col mt-3 mt-5 mb-5 tracking-tight">
+                            <span className="text-xl font-medium">21/06/2025 23:30</span>
+                            <span className="text-sm">Czas przylotu</span>
                         </div>
                     </div>
                 </div>
-            </div>
 
 
         </div>
